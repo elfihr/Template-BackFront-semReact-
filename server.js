74e1a14188d09a6fcec26d0760d8d56f2532cfb2
Luiz Felipe <70807899+elfihr@users.noreply.github.com>
2024-03-17 22:37:37 -0300

    correcaoTemplate

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const {middlewareGlobal,outroMidleware,checkCsrfError,csrfMiddleware} = require(
 
 
 app.use(helmet())//Ativa o helmet
-app.use(express.jason())//Parse de json pra dentro da aplicação
+app.use(express.json())//Parse de json pra dentro da aplicação
 app.use(express.urlencoded({extended:true}))
 
 app.use(express.static(path.resolve(__dirname, 'public')))//ajustando path no public
